f4b784c0911b99a22815dd96cfad32c5da2ab215
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-08-18 17:53:33 +0300

    commit tree 76b1a86ac0d7a23d67ee16ecf6b9baa9e1fcc77e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = ["Hello", "World"]
+def line array
+  if array == []
+    puts "The line is currently empty."
+  else 
+    new_arr = array.map.with_index {|name, idx| "#{idx + 1}. #{name}"}
+    # puts new_arr.join(" ")
+    puts "The line is currently: #{new_arr.join(" ")}"
+
+  end
+end
+
+def take_a_number array, name
+  array = array << name
+  # p array
+  puts "Welcome, #{array[-1]}. You are number #{array.length} in line."
+end
+
+def now_serving array
+  if array == []
+    puts "There is nobody waiting to be served!"
+  else  
+    puts "Currently serving #{array[0]}."
+    array.shift()
+  end
+end
